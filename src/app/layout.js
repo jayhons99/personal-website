7b563172efc338e974c7d37afaa2c9e934171085
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-05-25 15:48:45 -0400

    revert to previous

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import { motion, AnimatePresence } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 export const variants = {
@@ -11,17 +10,17 @@ export const variants = {
   exit: { opacity: 0, x: 0, y: 0 },
 };
 
-export const metadata = {
-  title: "Personal Website",
-  description: "Created by Jhon Sisneros using Nextjs",
-};
+// export const metadata = {
+//   title: "Personal Website",
+//   description: "Created by Jhon Sisneros using Nextjs",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-primary`}>
         <Navbar />
-        <AnimatePresence mode="wait">{children}</AnimatePresence>
+        {children}
       </body>
     </html>
   );
