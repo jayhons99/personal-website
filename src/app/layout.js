bf18083448eaa35d811d8b3240cf504855c93a3f
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-05-28 01:49:35 -0400

    fixed bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,24 @@
 "use client";
+
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Personal Website",
-  description: "Created by Jhon Sisneros using Nextjs",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Personal Website</title>
+        <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content="Personal website built by Jhon Sisneros"
+        />
+        <meta name="author" content="Jhon Sisneros" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={`${inter.className} bg-primary`}>
         <Navbar />
         {children}
