94f724ac903ecbd867a3896468af262539647f38
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-05-28 13:28:39 -0400

    hotfix; fixed footer display on mobile screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <p className="text-center text-sm absolute bottom-0 w-full text-gray-600 mb-8">
+      <p className="text-center text-sm relative bottom-0 w-full text-gray-600 pb-4 mt-8">
         &copy; 2023 Jhon Sisneros
       </p>
     </>
